4f6c4d05542849c72ab585f3fce5647eb702a018
sergii.berezhnyi <sergii_berezhnyi_tp@bmc.com>
2019-02-12 14:26:47 +0200

    Initial commit

---
+++ src/test/java/SimpleGoogleTest.java
@@ -0,0 +1,37 @@
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class SimpleGoogleTest {
+
+    private WebDriver driver;
+
+    @BeforeClass
+    public static void setupClass() {
+        WebDriverManager.chromedriver().setup();
+    }
+
+    @Before
+    public void setupTest() {
+        driver = new ChromeDriver();
+    }
+
+    @After
+    public void teardown() {
+        if (driver != null) {
+            driver.quit();
+        }
+    }
+
+    @Test
+    public void googleTEst(){
+        driver.get("https://google.com");
+        driver.findElement(By.name("q")).sendKeys("Selenium");
+        driver.findElement(By.name("btnK")).click();
+    }
+}
